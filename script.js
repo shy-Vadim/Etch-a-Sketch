13d79ee23af4cb0ee06f4a06532a2905c904c52d
Vadim J <136094345+shy-Vadim@users.noreply.github.com>
2024-02-02 14:31:45 +0300

    Changed class name

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const container = document.getElementsByClassName(`container`);
+const canvas = document.getElementsByClassName(`canvas`);
 for (i = 0 ; i < 16 * 16 ; i++) {
   const div = document.createElement("div");
-  div.classList.add('container-box');
-  container[0].appendChild(div);
+  div.classList.add('canvas-box');
+  canvas[0].appendChild(div);
 }
