1037d73c9e61fb6f6c8c69d58d69c0a0a876cb4f
Vadim J <136094345+shy-Vadim@users.noreply.github.com>
2024-02-02 23:34:48 +0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-const canvas = document.getElementsByClassName(`canvas`);
-for (i = 0 ; i < 16 * 16 ; i++) {
-  const div = document.createElement("div");
-  div.classList.add('canvas-box');
-  canvas[0].appendChild(div);
+// DOM SELECTORS
+const canvas = document.querySelector(`.canvas`);
+let canvasBoxesList = document.querySelectorAll('.canvas-box');
+
+// VARIABLES
+let currentGridSize = 16;
+let currentColor = "black";
+
+// FUNCTIONS
+function drawCanvasBoxes(gridSize) {
+  for (i = 0 ; i < gridSize ** 2 ; i++) {
+    const div = document.createElement("div");
+    div.classList.add('canvas-box');
+    canvas.appendChild(div);
+    div.addEventListener("click", () => {
+      changeElementBG(currentColor, div)
+    })
+  }
+  canvasBoxesList = document.querySelectorAll('.canvas-box');
 }
+
+function removeCanvasBoxes() {
+  let canvasBoxes = document.querySelectorAll('.canvas-box')
+  canvasBoxes.forEach(element => {
+    canvas.removeChild(element);
+  });
+}
+
+function changeElementBG(color, elementToChange) {
+  elementToChange.classList.add(color)
+}
+
+// EVENTS
+
+
+
+// OTHER STUFF
+drawCanvasBoxes(currentGridSize)
+
+
